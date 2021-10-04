3cf34e2f35fdb5c14c2054a812cf202b9ff699bd
Sendhilkumar Narasingam <snarasin@synamedia.com>
2021-10-04 12:12:30 +0530

    sending the file from server

--- client.go
+++ client.go
@@ -11,8 +11,11 @@ func main() {
 	fmt.Println("Start client...")
 
 	conn, _ := net.Dial("tcp", "127.0.0.1:9000")
-
+	buf := make([]byte, 16)
 	for {
+		//message, _ := bufio.NewReader(conn).ReadString('\n')
+		conn.Read(buf)
+		fmt.Print("Received :", buf)
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("Text to send: ")
 		text, _ := reader.ReadString('\n')
--- server.go
+++ server.go
@@ -3,7 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
+	"log"
 	"net"
+	"os"
 )
 
 func main() {
@@ -12,8 +15,31 @@ func main() {
 	ln, _ := net.Listen("tcp", ":9000")
 
 	conn, _ := ln.Accept()
+	fmt.Println("Accepted connection")
+
+	f, err := os.Open("fileread_write.go")
+
+	if err != nil {
+		log.Fatal("no file exists")
+	}
+
+	defer f.Close()
+
+	reader := bufio.NewReader(f)
+
+	buf := make([]byte, 16)
 
 	for {
+		_, err := reader.Read(buf)
+
+		if err != nil {
+			if err != io.EOF {
+				log.Fatal("error while reading")
+			}
+			break
+		}
+		//conn.Write(buf)
+		fmt.Fprint(conn, buf)
 		message, _ := bufio.NewReader(conn).ReadString('\n')
 		fmt.Printf("Message Receied %s\n", string(message))
 		if message == "end\n" {
